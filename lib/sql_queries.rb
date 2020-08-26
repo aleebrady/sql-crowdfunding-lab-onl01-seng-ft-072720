3d9f53a65f9c502ccdcac4f120437fb35d7e6085
Aaron Brady <aleebrady@gmail.com>
2020-08-26 17:50:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Select projects.category, pledges.amount From projects Inner Join pledges On pledges.project.id = projects.id Where projects.category = 'music';"
+  "Select projects.category, pledges.amount From projects Inner Join pledges On pledges.project_id = projects.id Where projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
