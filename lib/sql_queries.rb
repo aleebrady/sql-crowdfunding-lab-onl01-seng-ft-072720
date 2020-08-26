f30b2bf1ee4c46aa7ab5f91bb09cbf068f9010ba
Aaron Brady <aleebrady@gmail.com>
2020-08-26 17:44:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Select user.name, pledges.amount "
+  "Select user.name, pledges.amount From users Inner Join pledges On pledges.user_id = users.id Group By users.name Order by Sum(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
